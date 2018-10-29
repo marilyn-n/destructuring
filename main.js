c2497172371bb39df1627521e8045380cf02332d
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-10-28 20:26:57 -0600

    add data to obj

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const person = {
   alive: true,
   nationality: 'Mexican',
   adress: 'Magdalena Contreras, CDMX',
-  favFoods: ['Pozole', 'Enchiladas', 'Pollo', 'Pastel']
+  favFoods: ['Pozole', 'Enchiladas', 'Pollo', 'Pastel'],
+  favBooks: ['The green house', 'Maneater', 'Frankenstain', 'Food and Wine', 'The Elephant'],
+  data: [
+    { latitude: -094853.1 ,longitude: 153982.90, size: 'M', color: 'Gray' },
+    { latitude: -094853.1 ,longitude: 153982.90, size: 'S', color: 'Yellow' },
+    { latitude: -094853.1 ,longitude: 153982.90, size: 'XL', color: 'Blue' },
+    { latitude: -094853.1 ,longitude: 153982.90, size: 'L', color: 'Red' },
+    { latitude: -094853.1 ,longitude: 153982.90, size: 'XS', color: 'White'}
+  ]
 }
 
 const { favFoods } = person
@@ -25,4 +33,17 @@ console.log(a)
 
 const [ ,,,,,,,p = '10' ] = countries
 console.log(p)
+
+// get color and latitude
+
+const { data } = person
+
+const formatedData = data.map(item => {
+  return '* ' + item.color
+})
+
+console.log(formatedData)
+
+
+
 
